84befa62a12562a9d87ba35c903dbdd9c04caec5
Stephen Bowen <bowensteven17@gmail.com>
2024-05-21 11:28:56 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length==0:
+        print([])
+        return
+    elif length==1:
+        print([0])
+        return
+    else:
+        starting_sequence=[0,1]
+        while len(starting_sequence)<length:
+            next_number=starting_sequence[-1]+starting_sequence[-2]
+            starting_sequence.append(next_number)
+        print(starting_sequence)
